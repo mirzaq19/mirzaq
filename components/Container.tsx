c3eb60e13a0e5bef521861d754f53c0bfaf7a889
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2021-12-24 22:40:48 +0700

    update: container optional parameter

--- components/Container.tsx
+++ components/Container.tsx
@@ -1,10 +1,12 @@
 interface ContainerProps {
-  className: string;
+  className?: string;
 }
 
 const Container: React.FC<ContainerProps> = ({ children, className }) => {
   return (
-    <div className={`container mx-auto px-6 ${className}`}>{children}</div>
+    <div className={`container mx-auto px-6 ${className ?? ""}`}>
+      {children}
+    </div>
   );
 };
 
