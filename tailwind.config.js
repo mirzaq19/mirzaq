4b2d3471b2621c4a734f596d9dae7595d22fa953
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2021-12-24 16:44:03 +0700

    update: color schema

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,15 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        dark1: "#12002f",
+        dark2: "#1d0643",
+        dark3: "#270e50",
+        peach: "#ff56a5",
+        grape: "#672ae7",
+      },
+    },
   },
   plugins: [],
 };
