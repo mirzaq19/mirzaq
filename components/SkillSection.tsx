b1decf320e4f9d91cb2e6217e86a606d64e5c283
M. Auliya Mirzaq Romdloni <mirzaqarjap@gmail.com>
2022-02-09 20:48:27 +0700

    update: change images data with flex style

--- components/SkillSection.tsx
+++ components/SkillSection.tsx
@@ -8,6 +8,41 @@ import {
   faLaptopCode,
 } from "@fortawesome/free-solid-svg-icons";
 
+const images = [
+  {
+    src: "/assets/techstack/VScode_techstack.webp",
+    alt: "VS Code",
+  },
+  {
+    src: "/assets/techstack/HTML_techstack.webp",
+    alt: "HTML",
+  },
+  {
+    src: "/assets/techstack/CSS_techstack.webp",
+    alt: "CSS",
+  },
+  {
+    src: "/assets/techstack/Sass_techstack.webp",
+    alt: "SASS",
+  },
+  {
+    src: "/assets/techstack/Javascript_techstack.webp",
+    alt: "Javascript",
+  },
+  {
+    src: "/assets/techstack/React_techstack.webp",
+    alt: "React Js",
+  },
+  {
+    src: "/assets/techstack/Illustrator_techstack.webp",
+    alt: "Illustrator",
+  },
+  {
+    src: "/assets/techstack/Figma_techstack.webp",
+    alt: "Figma",
+  },
+];
+
 export default function SkillSection() {
   return (
     <div className="bg-dark1" id="skill">
@@ -18,87 +53,19 @@ export default function SkillSection() {
           development that I usually use. Below are some of the ones I usually
           use
         </p>
-        <div className="grid grid-cols-3 px-10 justify-center sm:grid-cols-4 md:grid-cols-5 lg:grid-cols-6 gap-10 xl:gap-16 xl:px-20 2xl:px-56">
-          <span>
-            <Image
-              src="/assets/techstack/VScode_techstack.webp"
-              alt="VS Code"
-              layout="responsive"
-              draggable={false}
-              width={50}
-              height={50}
-            />
-          </span>
-          <span>
-            <Image
-              src="/assets/techstack/HTML_techstack.webp"
-              alt="HTML"
-              layout="responsive"
-              draggable={false}
-              width={50}
-              height={50}
-            />
-          </span>
-          <span>
-            <Image
-              src="/assets/techstack/CSS_techstack.webp"
-              alt="CSS"
-              layout="responsive"
-              draggable={false}
-              width={50}
-              height={50}
-            />
-          </span>
-          <span>
-            <Image
-              src="/assets/techstack/Sass_techstack.webp"
-              alt="SASS"
-              layout="responsive"
-              draggable={false}
-              width={50}
-              height={50}
-            />
-          </span>
-          <span>
-            <Image
-              src="/assets/techstack/Javascript_techstack.webp"
-              alt="JS"
-              layout="responsive"
-              draggable={false}
-              width={50}
-              height={50}
-            />
-          </span>
-          <span className="md:col-start-2 lg:col-start-auto">
-            <Image
-              src="/assets/techstack/React_techstack.webp"
-              alt="React"
-              layout="responsive"
-              draggable={false}
-              width={50}
-              height={50}
-            />
-          </span>
-          <span className="lg:col-start-3">
-            <Image
-              src="/assets/techstack/Illustrator_techstack.webp"
-              alt="Illustrator"
-              layout="responsive"
-              draggable={false}
-              width={50}
-              height={50}
-            />
-          </span>
-          <span>
-            <Image
-              src="/assets/techstack/Figma_techstack.webp"
-              alt="Figma"
-              layout="responsive"
-              draggable={false}
-              width={50}
-              height={50}
-            />
-          </span>
+        <div className="flex justify-center gap-10 flex-wrap px-10 xl:px-60">
+          {images.map((image, idx) => (
+            <span className="w-20 lg:w-24 xl:w-28" key={idx}>
+              <Image
+                src={image.src}
+                alt={image.alt}
+                layout="responsive"
+                draggable={false}
+                width={80}
+                height={80}
+              />
+            </span>
+          ))}
         </div>
         <div className="grid grid-cols-1 lg:grid-cols-3 my-20 px-10 gap-y-14 sm:gap-y-20 sm:pt-12 lg:gap-y-0 lg:flex-row lg:pt-14 lg:gap-x-10 xl:gap-x-20 xl:px-20 2xl:px-52">
           <CardSkill title="Graphic Design" icon={faDrawPolygon}>
